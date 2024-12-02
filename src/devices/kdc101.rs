31eab7404581e7c334dfe7cc8724db5765c8a2ea
MillieFD <155637089+MillieFD@users.noreply.github.com>
2024-12-02 20:20:39 +0000

    Implemented UnitConversion, ChanEnableState, and Motor traits to the KDC101 device.

--- src/devices/kdc101.rs
+++ src/devices/kdc101.rs
@@ -10,7 +10,7 @@ Notes:
 */
 
 use crate::devices::usb_device_primitive::UsbDevicePrimitive;
-use crate::traits::ThorlabsDevice;
+use crate::traits::{ChanEnableState, Motor, ThorlabsDevice, UnitConversion};
 use std::ops::Deref;
 
 #[derive(Debug)]
@@ -34,3 +34,13 @@ impl Deref for KDC101 {
         &self.device
     }
 }
+
+impl UnitConversion for KDC101 {
+    const DISTANCE_ANGLE_SCALING_FACTOR: f64 = 34554.96;
+    const VELOCITY_SCALING_FACTOR: f64 = 772981.3692;
+    const ACCELERATION_SCALING_FACTOR: f64 = 263.8443072;
+}
+
+impl ChanEnableState for KDC101 {}
+
+impl Motor for KDC101 {}
