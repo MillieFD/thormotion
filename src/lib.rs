6bbff0572e62c26da0fe682d5f5e31a7ff9c6ddf
MillieFD <155637089+MillieFD@users.noreply.github.com>
2024-12-02 20:21:54 +0000

    Restrict module visibility to crate
    Changed the visibility of several modules from public to private within the library. Updated tests to include additional asynchronous methods and device operations for improved functionality.

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,34 @@
-pub mod devices;
-pub mod enumerate;
+mod devices;
+mod enumerate;
 mod env;
-pub mod error;
+mod error;
 mod messages;
 mod traits;
 
-pub use error::Error;
+use error::Error;
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::devices::KDC101;
-    use crate::traits::ThorlabsDevice;
+    use crate::traits::{ChanEnableState, Motor, ThorlabsDevice};
+    use std::time::Duration;
+    use tokio::time::sleep;
 
     #[tokio::test]
     async fn identify_device_test() -> Result<(), Error> {
         let device = KDC101::new("27266788")?;
-        device.identify()?;
+        device.set_channel_enable_state(1, true).await?;
+        device.home(1).await?;
+        device.move_absolute(1, 1.0).await?;
+        sleep(Duration::from_secs(1)).await;
+        device.move_absolute(1, 0.0).await?;
+
+        // let device = KDC101::new("27264344")?;
+        // device.identify()?;
+        //
+        // let device = KDC101::new("27266825")?;
+        // device.identify()?;
         Ok(())
     }
 }
