8f00d95b5e18af551773b7f926e4e67662608f6c
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-01-17 16:24:16 +0000

    Moved MsgFormat enum from `traits.rs` to `messages.rs` to improve organisation

--- src/messages.rs
+++ src/messages.rs
@@ -10,9 +10,10 @@ hashmaps at compile-time. Functions are provided to look up the metadata and gro
 ---------------------------------------------------------------------------------------------------
 Notes:
 */
-
 use crate::error::Error;
 use crate::messages::ChannelStatus::{New, Sub};
+use std::fmt::{Display, Formatter};
+use std::ops::Deref;
 use std::sync::RwLock;
 use tokio::sync::broadcast::{channel, Receiver, Sender};
 
@@ -74,5 +75,78 @@ pub(crate) fn get_rx_new_or_err(id: [u8; 2]) -> Result<Receiver<Box<[u8]>>, Erro
         let (tx, rx) = channel(1);
         waiting_sender.replace(tx);
         Ok(rx)
+    }
+}
+
+/// # Message Format
+/// The Thorlabs APT communication protocol uses a fixed length 6-byte message header, which may
+/// be followed by a variable-length data packet. For simple commands, the 6-byte message header
+/// is sufficient to convey the entire command. For more complex commands (e.g. commands where a
+/// set of parameters needs to be passed to the device) the 6-byte header is insufficient and
+/// must be followed by a data packet. The `MsgFormat` enum is used to wrap the bytes of a message
+/// and indicate whether the message is `Short` (six byte header only) or `Long` (six byte header
+/// plus variable length data package).
+
+pub enum MsgFormat {
+    Short([u8; 6]),
+    Long(Vec<u8>),
+}
+
+impl Deref for MsgFormat {
+    type Target = [u8];
+    fn deref(&self) -> &Self::Target {
+        match self {
+            MsgFormat::Short(arr) => arr,
+            MsgFormat::Long(vec) => vec.as_slice(),
+        }
+    }
+}
+
+impl Extend<u8> for MsgFormat {
+    fn extend<T: IntoIterator<Item = u8>>(&mut self, iter: T) {
+        match self {
+            MsgFormat::Short(arr) => {
+                let mut vec = arr.to_vec();
+                vec.extend(iter);
+                *self = MsgFormat::Long(vec);
+            }
+            MsgFormat::Long(vec) => vec.extend(iter),
+        }
+    }
+}
+
+impl Display for MsgFormat {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            MsgFormat::Short(arr) => {
+                write!(
+                    f,
+                    "Short Message [ {} ]",
+                    arr.iter()
+                        .map(|b| format!("{:02X}", b))
+                        .collect::<Vec<String>>()
+                        .join(" ")
+                )
+            }
+            MsgFormat::Long(vec) => {
+                write!(
+                    f,
+                    "Long Message [ {} ]",
+                    vec.iter()
+                        .map(|b| format!("{:02X}", b))
+                        .collect::<Vec<String>>()
+                        .join(" ")
+                )
+            }
+        }
+    }
+}
+
+impl MsgFormat {
+    pub(crate) fn len(&self) -> usize {
+        match self {
+            MsgFormat::Short(arr) => arr.len(),
+            MsgFormat::Long(vec) => vec.len(),
+        }
     }
 }
