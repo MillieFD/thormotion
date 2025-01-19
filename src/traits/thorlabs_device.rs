dacda93acff4a9bac985b9df5463c416abb16f27
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-01-19 22:58:04 +0000

    Refactor `ThorlabsDevice` trait for improved API clarity
    Streamlined error handling, added traits (`From`, `Display`), and enhanced documentation for APT protocol compliance. Simplified hardware info parsing. Removed redundant helper methods.

--- src/traits/thorlabs_device.rs
+++ src/traits/thorlabs_device.rs
@@ -2,14 +2,17 @@
 Project: thormotion
 GitHub: https://github.com/MillieFD/thormotion
 License: BSD 3-Clause "New" or "Revised" License, Copyright (c) 2025, Amelia Fraser-Dale
-Filename: test.rs
+Filename: thorlabs_device.rs
 */
 
 use crate::devices::{HardwareInfo, UsbDevicePrimitive};
 use crate::env::{DEST, LONG_TIMEOUT, SOURCE};
+use crate::error::AptError::EnumerationError;
 use crate::error::Error;
+use crate::error::Error::AptError;
 use crate::messages::ChannelStatus::{New, Sub};
 use crate::messages::{get_rx_new_or_sub, MsgFormat};
+use std::fmt::Display;
 use std::ops::Deref;
 use tokio::time::timeout;
 
@@ -18,7 +21,13 @@ use tokio::time::timeout;
 /// It defines functions which are common to all Thorlabs devices,
 /// including functions to simplify communication using the APT protocol.
 pub trait ThorlabsDevice:
-    From<UsbDevicePrimitive> + Deref<Target = UsbDevicePrimitive> + Send + Sync
+    From<UsbDevicePrimitive>
+    + From<String>
+    + From<&'static str>
+    + Deref<Target = UsbDevicePrimitive>
+    + Display
+    + Send
+    + Sync
 {
     fn new(serial_number: &str) -> Result<Self, Error>;
 
@@ -33,11 +42,12 @@ pub trait ThorlabsDevice:
         if serial_number.starts_with(Self::SERIAL_NUMBER_PREFIX) {
             Ok(())
         } else {
-            Err(Error::EnumerationError(format!(
-                "Serial number {} is not valid for the selected device type. Expected a serial number starting with {}",
+            Err(AptError(EnumerationError(format!(
+                "Serial number {} is not valid for the selected device type. \
+                Expected a serial number starting with {}",
                 serial_number,
                 Self::SERIAL_NUMBER_PREFIX,
-            )))
+            ))))
         }
     }
 
@@ -54,7 +64,7 @@ pub trait ThorlabsDevice:
     /// data package).
     ///
     /// The `pack_short_message()` and `pack_long_message()` helper functions are implemented to
-    /// simplify message formatting and enforce consistency with the APT protocol.  
+    /// simplify message formatting and enforce consistency with the APT protocol.
     fn pack_short_message(id: [u8; 2], param1: u8, param2: u8) -> MsgFormat {
         MsgFormat::Short([id[0], id[1], param1, param2, DEST, SOURCE])
     }
@@ -81,6 +91,10 @@ pub trait ThorlabsDevice:
     /// channel to be identified. In single-channel controllers, the Channel Ident byte is
     /// ignored as the destination of the command is uniquely identified by the USB serial
     /// number of the controller.
+    ///
+    /// Message ID: 0x0223
+    ///
+    /// Message Length: 6 bytes (header only)
     fn identify(&self) -> Result<(), Error> {
         const ID: [u8; 2] = [0x23, 0x02];
         let data = Self::pack_short_message(ID, 0, 0);
@@ -94,11 +108,18 @@ pub trait ThorlabsDevice:
     ///
     /// This function starts automatic status updates from the embedded controller. Status
     /// update messages contain information about the position and status of the controller
-    /// (such as limit switch status or current position). The messages will be sent by the
-    /// controller every 100 milliseconds (10 Hz) until the controller receives a
-    /// `HW_STOP_UPDATEMSGS` command. The same status information can also be requested at
-    /// a single time point (as a one-off rather than every 100 milliseconds) using the
-    /// controller's relevant `GET_STATUTSUPDATES` function.
+    /// (such as limit switch status or current position).
+    ///
+    /// Message ID: 0x0011
+    ///
+    /// Message Length: 6 bytes (header only)
+    ///
+    /// # Response
+    ///
+    /// The controller will send a status update message every 100 milliseconds (10 Hz) until
+    /// the receiving a `HW_STOP_UPDATEMSGS` command. The same status information can also be
+    /// requested at a single time point (as a one-off rather than every 100 milliseconds)
+    /// using the controller's relevant `GET_STATUTSUPDATE` function.
     fn start_update_messages(&self) -> Result<(), Error> {
         const ID: [u8; 2] = [0x11, 0x00];
         let data = Self::pack_short_message(ID, 0, 0);
@@ -110,16 +131,18 @@ pub trait ThorlabsDevice:
     ///
     /// **Function implemented from Thorlabs APT protocol**
     ///
-    /// This function stops automatic status updates from the embedded controller. Status
-    /// update messages contain information about the position and status of the controller
-    /// (such as limit switch status or current position). After receiving a
-    /// `HW_START_UPDATEMSGS` command, the controller will send a status message every
-    /// 100 milliseconds (10 Hz) until the controller receives a `HW_STOP_UPDATEMSGS`
-    /// command.
-    ///
+    /// This function stops automatic status updates from the embedded controller.
     /// This function is normally called by client applications when shutting down, to
     /// instruct the controller to turn off status updates to prevent USB buffer overflows
     /// on the PC.
+    ///
+    /// Message ID: 0x0012
+    ///
+    /// Message Length: 6 bytes (header only)
+    ///
+    /// # Response
+    ///
+    /// The controller will stop sending automatic status messages every 100 milliseconds (10 Hz).
     fn stop_update_messages(&self) -> Result<(), Error> {
         const ID: [u8; 2] = [0x12, 0x00];
         let data = Self::pack_short_message(ID, 0, 0);
@@ -132,10 +155,20 @@ pub trait ThorlabsDevice:
     /// **Function implemented from Thorlabs APT protocol**
     ///
     /// This function is used to request hardware information from the controller.
+    ///
+    /// Message ID: 0x0005
+    ///
+    /// Message length: 6 bytes (header only)
+    ///
+    /// # Response
+    ///
     /// The controller will send a `HW_GET_INFO (0x0006)` message in response, which
     /// is then parsed into a new instance of the `HardwareInfo` struct.
-    // todo This internal function is not intended to be used directly.
-    async fn req_hw_info(&self) -> Result<HardwareInfo, Error> {
+    ///
+    /// Response ID: 0x0006
+    ///
+    /// Response length: 90 bytes (6-byte header followed by 84-byte data packet)
+    async fn hw_req_info(&self) -> Result<HardwareInfo, Error> {
         const ID: [u8; 2] = [0x00, 0x05];
         let mut rx = match get_rx_new_or_sub(ID)? {
             Sub(rx) => rx,
@@ -146,65 +179,23 @@ pub trait ThorlabsDevice:
             }
         };
         let response = timeout(LONG_TIMEOUT, rx.recv()).await??;
-
-        // Parse response todo why all of these .try_into(). Why .unwrap() instead of `?`
-        let hardware_serial_number = u32::from_le_bytes(response[6..10].try_into().unwrap());
+        let serial_number = u32::from_le_bytes(response[6..10].try_into()?);
         let model_number = String::from_utf8_lossy(&response[10..18]).to_string();
-        let hardware_type = u16::from_le_bytes(response[18..20].try_into().unwrap());
-        let firmware_minor_revision = u8::from_le_bytes(response[20..21].try_into().unwrap());
-        let firmware_interim_revision = u8::from_le_bytes(response[21..22].try_into().unwrap());
-        let firmware_major_revision = u8::from_le_bytes(response[22..23].try_into().unwrap());
-        let firmware_version: String = format!(
-            "{}.{}.{}",
-            firmware_major_revision, firmware_interim_revision, firmware_minor_revision
-        );
-        let hardware_version = u16::from_le_bytes(response[84..86].try_into().unwrap());
-        let mod_state = u16::from_le_bytes(response[86..88].try_into().unwrap());
-        let number_channels = u16::from_le_bytes(response[88..90].try_into().unwrap());
-
+        let hardware_type = u16::from_le_bytes(response[18..20].try_into()?);
+        let firmware_minor = u8::from_le_bytes(response[20..21].try_into()?);
+        let firmware_interim = u8::from_le_bytes(response[21..22].try_into()?);
+        let firmware_major = u8::from_le_bytes(response[22..23].try_into()?);
+        let hardware_version = u16::from_le_bytes(response[84..86].try_into()?);
+        let mod_state = u16::from_le_bytes(response[86..88].try_into()?);
+        let number_channels = u16::from_le_bytes(response[88..90].try_into()?);
         Ok(HardwareInfo {
-            hardware_serial_number,
+            serial_number,
             model_number,
             hardware_type,
-            firmware_version,
+            firmware_version: format!("{}.{}.{}", firmware_major, firmware_interim, firmware_minor),
             hardware_version,
             mod_state,
             number_channels,
         })
-    }
-
-    async fn req_serial_number(&self) -> Result<u32, Error> {
-        let hw_info = self.req_hw_info().await?;
-        Ok(hw_info.hardware_serial_number)
-    }
-
-    async fn req_model_number(&self) -> Result<String, Error> {
-        let hw_info = self.req_hw_info().await?;
-        Ok(hw_info.model_number)
-    }
-
-    async fn req_hardware_type(&self) -> Result<u16, Error> {
-        let hw_info = self.req_hw_info().await?;
-        Ok(hw_info.hardware_type)
-    }
-
-    async fn req_firmware_version(&self) -> Result<String, Error> {
-        let hw_info = self.req_hw_info().await?;
-        Ok(hw_info.firmware_version)
-    }
-
-    async fn req_hardware_version(&self) -> Result<u16, Error> {
-        let hw_info = self.req_hw_info().await?;
-        Ok(hw_info.hardware_version)
-    }
-
-    async fn req_mod_state(&self) -> Result<u16, Error> {
-        let hw_info = self.req_hw_info().await?;
-        Ok(hw_info.mod_state)
-    }
-
-    async fn req_num_channels(&self) -> Result<u16, Error> {
-        let hw_info = self.req_hw_info().await?;
-        Ok(hw_info.number_channels)
     }
 }
