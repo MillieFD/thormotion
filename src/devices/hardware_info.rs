af40dee6a41165e14141f13a2b76893013470610
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-01-19 22:58:25 +0000

    Implement `Display` for `HardwareInfo` and update field names. Added a `Display` trait implementation to format `HardwareInfo` details. Renamed `hardware_serial_number` to `serial_number` for consistency.

--- src/devices/hardware_info.rs
+++ src/devices/hardware_info.rs
@@ -1,13 +1,11 @@
 /*
 Project: thormotion
 GitHub: https://github.com/MillieFD/thormotion
-Author: Amelia Fraser-Dale
-License: BSD 3-Clause "New" or "Revised"
+License: BSD 3-Clause "New" or "Revised" License, Copyright (c) 2025, Amelia Fraser-Dale
 Filename: hardware_info.rs
-Description: todo
----------------------------------------------------------------------------------------------------
-Notes:
 */
+
+use std::fmt::{Display, Formatter, Result as FmtResult};
 
 /// # Hardware Information
 /// The `HardwareInfo` struct is used to package information about the hardware of the
@@ -16,13 +14,34 @@ Notes:
 /// as part of the `ThorlabsDevice` trait.
 
 #[derive(Debug, Clone)]
-pub struct HardwareInfo {
-    // todo impl Display for HardwareInfo to show all the sections with names
-    pub(crate) hardware_serial_number: u32,
+pub(crate) struct HardwareInfo {
+    pub(crate) serial_number: u32,
     pub(crate) model_number: String,
     pub(crate) hardware_type: u16,
     pub(crate) firmware_version: String,
     pub(crate) hardware_version: u16,
     pub(crate) mod_state: u16,
     pub(crate) number_channels: u16,
+}
+
+impl Display for HardwareInfo {
+    fn fmt(&self, f: &mut Formatter<'_>) -> FmtResult {
+        write!(
+            f,
+            "Serial Number: {}\n\
+            Model Number: {}\n\
+            Hardware Type: {}\n\
+            Firmware Version: {}\n\
+            Hardware Version: {}\n\
+            Module State: {}\n\
+            Number of Channels: {}",
+            self.serial_number,
+            self.model_number,
+            self.hardware_type,
+            self.firmware_version,
+            self.hardware_version,
+            self.mod_state,
+            self.number_channels,
+        )
+    }
 }
