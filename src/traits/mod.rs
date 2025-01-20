faf5cea4d749b6f4db24c728b28d946ce85f9a74
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-01-20 17:12:52 +0000

    Remove ChannelEnableState trait and its references
    Deleted `ChannelEnableState` trait implementation from `channel_enable_state.rs` and removed its inclusion in `traits/mod.rs` to clean up unused code.

--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -5,17 +5,10 @@ License: BSD 3-Clause "New" or "Revised" License, Copyright (c) 2025, Amelia Fra
 Filename: traits/mod.rs
 */
 
-/// # Traits Module
-/// This module defines traits for controlling different Thorlabs devices.
-///
-/// Each trait contains functions which can be called by Thorlabs devices that implement
-/// the trait. This keeps the code modular and easier to maintain.
-mod channel_enable_state;
 mod hub;
 mod motor;
 mod thorlabs_device;
 
-pub use channel_enable_state::ChannelEnableState;
 pub use hub::Hub;
 pub use motor::Motor;
 pub use thorlabs_device::ThorlabsDevice;
