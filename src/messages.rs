cb66454783e9cf56a63d45e862b5e9aeb414611c
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-01-20 00:31:00 +0000

    Refactor error handling with ThormotionError wrapper
    Replaced direct usage of `Error::AptProtocolError` with the new `ThormotionError(AptProtocolError)` wrapper for consistency. Updated imports and cleaned comments.

--- src/messages.rs
+++ src/messages.rs
@@ -1,16 +1,11 @@
 /*
 Project: thormotion
 GitHub: https://github.com/MillieFD/thormotion
-Author: Amelia Fraser-Dale
-License: BSD 3-Clause "New" or "Revised"
-Filename: messages
-Description: This file contains the master-list of all MessageMetadata and MessageGroup instances.
-These static instances are used to populate the ALL_MESSAGE_METADATA and ALL_MESSAGE_GROUPS
-hashmaps at compile-time. Functions are provided to look up the metadata and group for a given ID.
----------------------------------------------------------------------------------------------------
-Notes:
+License: BSD 3-Clause "New" or "Revised" License, Copyright (c) 2025, Amelia Fraser-Dale
+Filename: messages.rs
 */
-use crate::error::Error;
+
+use crate::error::{AptProtocolError, Error, ThormotionError};
 use crate::messages::ChannelStatus::{New, Sub};
 use std::fmt::{Display, Formatter};
 use std::ops::Deref;
@@ -28,10 +23,10 @@ pub(crate) fn get_length(id: [u8; 2]) -> Result<usize, Error> {
     Ok(LENGTH_MAP
         .get(&id)
         .ok_or_else(|| {
-            Error::AptProtocolError(format!(
+            ThormotionError(AptProtocolError(format!(
                 "{:?} does not correspond to a known message ID",
                 id
-            ))
+            )))
         })?
         .clone())
 }
@@ -40,10 +35,10 @@ pub(crate) fn get_waiting_sender<'a>(
     id: [u8; 2],
 ) -> Result<&'a &'a RwLock<Option<Sender<Box<[u8]>>>>, Error> {
     Ok(SENDER_MAP.get(&id).ok_or_else(|| {
-        Error::AptProtocolError(format!(
+        ThormotionError(AptProtocolError(format!(
             "{:?} does not correspond to a known message ID",
             id
-        ))
+        )))
     })?)
 }
 
@@ -67,10 +62,10 @@ pub(crate) fn get_rx_new_or_sub(id: [u8; 2]) -> Result<ChannelStatus, Error> {
 pub(crate) fn get_rx_new_or_err(id: [u8; 2]) -> Result<Receiver<Box<[u8]>>, Error> {
     let mut waiting_sender = get_waiting_sender(id)?.write()?;
     if let Some(_) = waiting_sender.as_ref() {
-        Err(Error::AptProtocolError(format!(
+        Err(ThormotionError(AptProtocolError(format!(
             "A waiting sender already exists for message ID {:?}",
             id
-        )))
+        ))))
     } else {
         let (tx, rx) = channel(1);
         waiting_sender.replace(tx);
