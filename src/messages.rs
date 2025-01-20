6411cbf80ac0d3393a59ee87600e61de51f1e8ab
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-01-20 10:23:01 +0000

    "Remove ThormotionError usage and simplify error handling"
    Replaced occurrences of `ThormotionError` with `AptProtocolError` to streamline error types and enhance consistency across the codebase.

--- src/messages.rs
+++ src/messages.rs
@@ -5,7 +5,7 @@ License: BSD 3-Clause "New" or "Revised" License, Copyright (c) 2025, Amelia Fra
 Filename: messages.rs
 */
 
-use crate::error::{AptProtocolError, Error, ThormotionError};
+use crate::error::{AptProtocolError, Error};
 use crate::messages::ChannelStatus::{New, Sub};
 use std::fmt::{Display, Formatter};
 use std::ops::Deref;
@@ -23,10 +23,10 @@ pub(crate) fn get_length(id: [u8; 2]) -> Result<usize, Error> {
     Ok(LENGTH_MAP
         .get(&id)
         .ok_or_else(|| {
-            ThormotionError(AptProtocolError(format!(
+            AptProtocolError(format!(
                 "{:?} does not correspond to a known message ID",
                 id
-            )))
+            ))
         })?
         .clone())
 }
@@ -35,10 +35,10 @@ pub(crate) fn get_waiting_sender<'a>(
     id: [u8; 2],
 ) -> Result<&'a &'a RwLock<Option<Sender<Box<[u8]>>>>, Error> {
     Ok(SENDER_MAP.get(&id).ok_or_else(|| {
-        ThormotionError(AptProtocolError(format!(
+        AptProtocolError(format!(
             "{:?} does not correspond to a known message ID",
             id
-        )))
+        ))
     })?)
 }
 
@@ -62,10 +62,10 @@ pub(crate) fn get_rx_new_or_sub(id: [u8; 2]) -> Result<ChannelStatus, Error> {
 pub(crate) fn get_rx_new_or_err(id: [u8; 2]) -> Result<Receiver<Box<[u8]>>, Error> {
     let mut waiting_sender = get_waiting_sender(id)?.write()?;
     if let Some(_) = waiting_sender.as_ref() {
-        Err(ThormotionError(AptProtocolError(format!(
+        Err(AptProtocolError(format!(
             "A waiting sender already exists for message ID {:?}",
             id
-        ))))
+        )))
     } else {
         let (tx, rx) = channel(1);
         waiting_sender.replace(tx);
