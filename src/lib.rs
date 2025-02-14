4dfee392bf666daad3ca80a0de880eff60de5e15
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-02-14 16:27:23 +0000

    Temporarily removed tests

--- src/lib.rs
+++ src/lib.rs
@@ -43,28 +43,11 @@ fn thormotion(module: &Bound<'_, PyModule>) -> PyResult<()> {
     Ok(())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::devices::KDC101;
-    use crate::traits::{ChanEnableState, Motor, ThorlabsDevice};
-    use std::time::Duration;
-    use tokio::time::sleep;
-
-    #[tokio::test]
-    async fn identify_device_test() -> Result<(), Error> {
-        let device = KDC101::new("27266788")?;
-        device.set_channel_enable_state(1, true).await?;
-        device.home(1).await?;
-        device.move_absolute(1, 1.0).await?;
-        sleep(Duration::from_secs(1)).await;
-        device.move_absolute(1, 0.0).await?;
-
-        // let device = KDC101::new("27264344")?;
-        // device.identify()?;
-        //
-        // let device = KDC101::new("27266825")?;
-        // device.identify()?;
-        Ok(())
-    }
-}
+/*
+todo write tests which cycle through all templates
+create test device struct with implements every trait
+without checking serial number
+separate test for each function
+failed test skips to next test without stopping the whole process
+output to csv
+ */
