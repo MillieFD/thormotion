e80dc3e6cd3088eb85cde10083bb869310c1d4b3
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-04-01 17:22:39 +0100

    Refactor `ThorlabsDevice` trait and enhance functionality
    Introduced new methods (`inner`, `as_dyn`, `as_str`, `abort`) to expand the usability of the `ThorlabsDevice` trait, enabling dynamic dispatch and improved device management. Added `Hash`, `PartialEq`, and `Eq` implementations for better handling of trait objects. These changes simplify integration and enhance flexibility when working with Thorlabs devices.
    
    Signed-off-by: MillieFD <155637089+MillieFD@users.noreply.github.com>

--- src/traits/thorlabs_device.rs
+++ src/traits/thorlabs_device.rs
@@ -30,8 +30,54 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-use nusb::Device;
 use std::fmt::Display;
-use std::ops::Deref;
+use std::hash::{Hash, Hasher};
+use std::sync::Arc;
 
-pub trait ThorlabsDevice: Display + Send + Sync + Deref<Target = Device> {}
+use crate::devices::UsbPrimitive;
+
+pub trait ThorlabsDevice: Display + Send + Sync {
+    /// Returns a borrow which dereferences to the inner [`UsbPrimitive`]
+    fn inner(&self) -> &UsbPrimitive;
+
+    /// Returns an [`Arc`] wrapping a dynamic trait object.
+    ///
+    /// This simplifies dynamic dispatch for code which can act on any
+    /// [`Thorlabs Device`][ThorlabsDevice] type.
+    fn as_dyn(&self) -> Arc<dyn ThorlabsDevice> {
+        Arc::clone(self)
+    }
+
+    /// Returns a `&str` representing the device name, serial number, and current status.
+    fn as_str(&self) -> &str;
+
+    /// Safely brings the [`USB Device`][UsbPrimitive] to a resting state and releases the claimed
+    /// [`Interface`][nusb::Interface].
+    ///
+    /// No action is taken if the device [`Status`][1] is already [`Closed`][2].
+    ///
+    /// Does not remove the device from the [`Global Device Manager`][3].
+    /// You can use `open` to resume communication.
+    ///
+    /// To release the claimed [`Interface`][nusb::Interface] without bringing the device to a
+    /// resting state, use `close`.
+    ///
+    /// [1]: crate::devices::usb_primitive::status::Status
+    /// [2]: crate::devices::usb_primitive::status::Status::Closed
+    /// [3]: crate::devices::device_manager::DeviceManager
+    fn abort(&self);
+}
+
+impl Hash for dyn ThorlabsDevice {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.inner().serial_number().hash(state);
+    }
+}
+
+impl PartialEq for dyn ThorlabsDevice {
+    fn eq(&self, other: &Self) -> bool {
+        self.inner().serial_number() == other.serial_number()
+    }
+}
+
+impl Eq for dyn ThorlabsDevice {}
