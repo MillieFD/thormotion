abece41ae44fac9618fed95094f4c5ec627f2970
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-04-01 15:11:16 +0100

    Refactor USB communicator for clarity and error handling
    Revised comments and naming to improve clarity, emphasizing commands over messages for consistency with protocol semantics. Introduced dedicated `TransferError` handling with `handle_error`, enhancing error reporting by invoking `abort`. Updated async task logic for better modularity and future extensibility.
    
    Signed-off-by: MillieFD <155637089+MillieFD@users.noreply.github.com>

--- src/devices/usb_primitive/communicator.rs
+++ src/devices/usb_primitive/communicator.rs
@@ -31,23 +31,26 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
 use nusb::Interface;
-use nusb::transfer::{Queue, RequestBuffer};
+use nusb::transfer::{Queue, RequestBuffer, TransferError};
 use smol::Task;
 
+use crate::devices::abort;
 use crate::devices::usb_primitive::serial_port::init;
-use crate::error::Error;
 use crate::messages::Dispatcher;
 
+// SAFETY: Currently, no data packet exceeds 255 bytes (Thorlabs APT Protocol, Issue 38, Page 35).
+// The max possible command length is therefore six-bytes (header) plus 255 bytes (data packet).
+/// The maximum possible size for a Thorlabs APT command
 const BUFFER_SIZE: usize = 255 + 6;
 
-/// Handles all incoming and outgoing messages between the host and a specific USB [`Interface`].
+/// Handles all incoming and outgoing commands between the host and a specific USB [`Interface`].
 pub(super) struct Communicator {
     /// A thread-safe message [`Dispatcher`] for handling async `Req → Get` callback patterns.
     dispatcher: Dispatcher,
-    /// An async background task that handles a stream of incoming messages from the [`Interface`].
+    /// An async background task that handles a stream of incoming commands from the [`Interface`].
     incoming: Task<()>,
-    ///A [`Queue`] that handles a stream of outgoing messages to the USB [`Interface`].
-    outgoing: Queue<Vec<u8>>,
+    ///A [`Queue`] that handles a stream of outgoing commands to the USB [`Interface`].
+    pub(super) outgoing: Queue<Vec<u8>>,
 }
 
 impl Communicator {
@@ -64,17 +67,30 @@ impl Communicator {
         }
     }
 
-    fn handle_error(error: Error) {
+    /// Handles any [`TransferError`] returned from the [`incoming task`][Self::spawn].
+    ///
+    /// A `match` statement allows [`TransferError`] variants to be handled differently if required.
+    ///
+    /// If the incoming task terminates, it cannot be restarted without the [`Interface`].
+    /// Automatic recovery may be implemented in a future version of Thormotion.
+    fn handle_error(error: TransferError) {
         match error {
-            _ => panic!("{}", error),
+            _ => abort(format!("Background task error : {}", error)),
         }
     }
 
+    /// Spawns an async background task that handles a stream of incoming commands from the
+    /// [`Interface`].
+    ///
+    /// The task loops indefinitely until either:
+    /// 1. It is explicitly [`cancelled`][Task::cancel]
+    /// 2. The [`Communicator`] is dropped
+    /// 3. A [`TransferError`] occurs. See [`Self::handle_error`].
     fn spawn(interface: Interface, dispatcher: Dispatcher) -> Task<()> {
         const IN_ENDPOINT: u8 = 0x81;
         let mut queue = interface.interrupt_in_queue(IN_ENDPOINT);
 
-        let mut listen = async move || -> Result<(), Error> {
+        let mut listen = async move || -> Result<(), TransferError> {
             loop {
                 queue.submit(RequestBuffer::new(BUFFER_SIZE));
                 let completion = queue.next_complete().await;
@@ -90,8 +106,9 @@ impl Communicator {
         })
     }
 
-    fn send(&mut self, message: Vec<u8>) {
-        self.outgoing.submit(message);
+    /// Send a command to the device.
+    fn send(&mut self, command: Vec<u8>) {
+        self.outgoing.submit(command);
     }
 
     pub(super) fn get_dispatcher(&self) -> Dispatcher {
