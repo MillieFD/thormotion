3ee0a9e44758f4dd2b0dc16900eaff5942359ae0
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-04-03 19:29:24 +0100

    Add private module and re-exports for identify
    Introduced a new private module `identify` and its related re-exports to `mod.rs`. This aligns with the existing structure and enables internal usage of `__identify` and `__home` without affecting the public API.
    
    Signed-off-by: MillieFD <155637089+MillieFD@users.noreply.github.com>

--- src/functions/mod.rs
+++ src/functions/mod.rs
@@ -30,4 +30,12 @@ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+/* ----------------------------------------------------------------------------- Private Modules */
+
 mod home;
+mod identify;
+
+/* -------------------------------------------------------------------------- Private Re-Exports */
+
+pub(crate) use home::__home;
+pub(crate) use identify::__identify;
