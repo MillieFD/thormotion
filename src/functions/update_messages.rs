0443afeec0d727ba25b0b4da7eac1bdbaaf10545
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-06-29 13:02:43 +0100

    Remove channel parameter from `update_messages` functions.
    Refactored `__hw_start_update_messages` and `__hw_stop_update_messages` to no longer require the `channel` parameter. Simplified message construction by defaulting the channel field to 0, improving clarity and reducing potential misuse.
    
    Signed-off-by: MillieFD <155637089+MillieFD@users.noreply.github.com>

--- src/functions/update_messages.rs
+++ src/functions/update_messages.rs
@@ -15,25 +15,23 @@ use crate::traits::ThorlabsDevice;
 ///
 /// Automatic updates will continue until the `stop_update_messages` function is called.
 /// A 'one-off' status update can be requested using `get_status_update_async`.
-pub(crate) async fn __hw_start_update_messages<A>(device: &A, channel: u8)
+pub(crate) async fn __hw_start_update_messages<A>(device: &A)
 where
     A: ThorlabsDevice,
 {
     const ID: [u8; 2] = [0x11, 0x00];
-    device.check_channel(channel);
-    let command = short(ID, channel, 0);
+    let command = short(ID, 0, 0);
     device.inner().send(command).await;
 }
 
 /// Stops periodic update messages from the device every 100 milliseconds (10 Hz).
 ///
 /// Automatic updates will cease until the `start_update_messages` function is called.
-pub(crate) async fn __hw_stop_update_messages<A>(device: &A, channel: u8)
+pub(crate) async fn __hw_stop_update_messages<A>(device: &A)
 where
     A: ThorlabsDevice,
 {
     const ID: [u8; 2] = [0x12, 0x00];
-    device.check_channel(channel);
-    let command = short(ID, channel, 0);
+    let command = short(ID, 0, 0);
     device.inner().send(command).await;
 }
