e1e7445eda4c7524259fbbb3fea707e1f3a4e2c1
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-10-31 15:03:58 +0000

    Updated comment headings in `mod.rs` for consistency.
    Signed-off-by: MillieFD <155637089+MillieFD@users.noreply.github.com>

--- thormotion-rs/src/traits/mod.rs
+++ thormotion-rs/src/traits/mod.rs
@@ -8,18 +8,16 @@ Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the conditions of the LICENSE are met.
 */
 
-#![allow(non_snake_case)]
-
 /* ----------------------------------------------------------------------------- Private Modules */
 
 mod check_serial_number;
 mod thorlabs_device;
 mod unit_conversion;
 
-/* -------------------------------------------------------------------------- Private Re-Exports */
+/* ----------------------------------------------------------------------------- Private Exports */
 
 pub(crate) use check_serial_number::CheckSerialNumber;
-/* --------------------------------------------------------------------------- Public
- * Re-Exports */
+/* ------------------------------------------------------------------------------ Public
+ * Exports */
 pub use thorlabs_device::ThorlabsDevice;
 pub(crate) use unit_conversion::{UnitConversion, Units};
