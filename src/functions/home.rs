b2d3ff9203a3cd9bbb9cd4d2603416b0338f067e
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-11-02 17:57:54 +0000

    Refactored `home` function for enhanced type safety.
    Updated the function to use `A: ThorlabsDevice<CH>` and `const CH: usize`, improving compile-time validation. Adjusted channel handling and added comments for better code clarity.
    
    Signed-off-by: MillieFD <155637089+MillieFD@users.noreply.github.com>

--- src/functions/home.rs
+++ src/functions/home.rs
@@ -12,17 +12,17 @@ use crate::messages::utils::short;
 use crate::traits::ThorlabsDevice;
 
 #[doc = include_str!("../documentation/home.md")]
-pub(crate) async fn home<A>(device: &A, channel: u8)
+pub(crate) async fn home<A, const CH: usize>(device: &A, channel: usize)
 where
-    A: ThorlabsDevice,
+    A: ThorlabsDevice<CH>,
 {
     const HOME: [u8; 2] = [0x43, 0x04];
     const HOMED: [u8; 2] = [0x44, 0x04];
-
-    device.check_channel(channel);
-    let rx = device.inner().receiver(&HOMED).await;
+    // Subscribe to the GET broadcast channel
+    let rx = device.inner().receiver(&HOMED, channel).await;
     if rx.is_new() {
-        let command = short(HOME, channel, 0);
+        // No HOMED response pending from the device. Send new HOME command.
+        let command = short(HOME, channel as u8, 0);
         device.inner().send(command).await;
     }
     let _ = rx.receive().await; // No need to parse the response
