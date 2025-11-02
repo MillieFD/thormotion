d639f4604f2c21ad14950547c1b6b85701da34c1
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-11-02 18:05:29 +0000

    Moved REQ and GET constants outside `get_status_bits`.
    Relocated the constants to the outer scope for better reusability and to align with common constant placement across the codebase.
    
    Signed-off-by: MillieFD <155637089+MillieFD@users.noreply.github.com>

--- src/functions/status_bits.rs
+++ src/functions/status_bits.rs
@@ -11,13 +11,14 @@ modification, are permitted provided that the conditions of the LICENSE are met.
 use crate::ThorlabsDevice;
 use crate::messages::utils::short;
 
+const REQ: [u8; 2] = [0x29, 0x04];
+const GET: [u8; 2] = [0x2A, 0x04];
+
 #[doc = include_str!("../documentation/get_status_bits.md")]
 pub(crate) async fn get_status_bits<A, const CH: usize>(device: &A, channel: usize) -> u32
 where
     A: ThorlabsDevice<CH>,
 {
-    const REQ: [u8; 2] = [0x29, 0x04];
-    const GET: [u8; 2] = [0x2A, 0x04];
     // Subscribe to the GET broadcast channel
     let rx = device.inner().receiver(&GET, channel).await;
     if rx.is_new() {
