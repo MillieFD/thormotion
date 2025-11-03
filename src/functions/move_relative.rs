3ec71e2cb9781cfe4d4850b9612624748c370c03
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-11-03 15:08:29 +0000

    Added `move_relative` functions with detailed debug logging.
    Introduced `move_relative` and `move_relative_from_params` functions to handle relative movements on Thorlabs devices. Included comprehensive debug logs to improve traceability, covering command initiation, response handling, and success confirmation.
    
    Signed-off-by: MillieFD <155637089+MillieFD@users.noreply.github.com>

---
+++ src/functions/move_relative.rs
@@ -0,0 +1,73 @@
+/*
+Project: thormotion
+GitHub: https://github.com/MillieFD/thormotion
+
+BSD 3-Clause License, Copyright (c) 2025, Amelia Fraser-Dale
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the conditions of the LICENSE are met.
+*/
+
+use crate::functions;
+use crate::messages::utils::{long, short};
+use crate::traits::{ThorlabsDevice, UnitConversion, Units};
+
+const MOVE_RELATIVE: [u8; 2] = [0x48, 0x04];
+const MOVE_COMPLETED: [u8; 2] = [0x64, 0x04];
+
+#[doc = include_str!("../documentation/move_relative.md")]
+pub(crate) async fn move_relative<A, const CH: usize>(device: &A, channel: usize, distance: f64)
+where
+    A: ThorlabsDevice<CH> + UnitConversion,
+{
+    log::debug!("{device} CHANNEL {channel} MOVE_RELATIVE (requested)");
+    // Subscribe to MOVE_COMPLETED broadcast channel
+    let rx = device.inner().new_receiver(&MOVE_COMPLETED, channel).await;
+    {
+        // No MOVE_COMPLETED response pending from the device. Send MOVE_RELATIVE command.
+        log::debug!("{device} CHANNEL {channel} MOVE_RELATIVE (is new)");
+        let command = {
+            let mut data: Vec<u8> = Vec::with_capacity(6);
+            data.extend((channel as u16).to_le_bytes());
+            data.extend(Units::distance_from_f64::<A>(distance));
+            long(MOVE_RELATIVE, &data)
+        };
+        device.inner().send(command).await;
+    }
+    // Wait for MOVE_COMPLETED response
+    let response = rx.receive().await;
+    log::debug!("{device} CHANNEL {channel} MOVE_RELATIVE (responded)");
+    // Parse the MOVE_COMPLETED response
+    if response[8] != 0x02 {
+        // TODO: Is the device already settled when response is sent?
+        functions::until_settled(device, channel).await;
+    }
+    log::debug!("{device} CHANNEL {channel} MOVE_RELATIVE (success)");
+}
+
+#[doc = include_str!("../documentation/move_relative_from_params.md")]
+pub(crate) async fn move_relative_from_params<A, const CH: usize>(device: &A, channel: usize) -> f64
+where
+    A: ThorlabsDevice<CH> + UnitConversion,
+{
+    log::debug!("{device} CHANNEL {channel} MOVE_RELATIVE_FROM_PARAMS (requested)");
+    // Subscribe to MOVE_COMPLETED broadcast channel
+    let rx = device.inner().new_receiver(&MOVE_COMPLETED, channel).await;
+    {
+        // No MOVE_COMPLETED response pending from the device. Send MOVE_RELATIVE command.
+        log::debug!("{device} CHANNEL {channel} MOVE_RELATIVE_FROM_PARAMS (is new)");
+        let command = short(MOVE_RELATIVE, channel as u8, 0);
+        device.inner().send(command).await;
+    }
+    // Wait for MOVE_COMPLETED response
+    let response = rx.receive().await;
+    log::debug!("{device} CHANNEL {channel} MOVE_RELATIVE_FROM_PARAMS (responded)");
+    // Parse the MOVE_COMPLETED response
+    if response[8] != 0x02 {
+        // TODO: Is the device already settled when response is sent?
+        functions::until_settled(device, channel).await;
+    }
+    log::debug!("{device} CHANNEL {channel} MOVE_RELATIVE (success)");
+    // Return the end position of the movement
+    device.decode(Units::distance_from_slice(&response[8..12]))
+}
