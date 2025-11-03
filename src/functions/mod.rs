9dc641acf023fec54c6d17f91886a720e9d303b7
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-11-03 15:32:59 +0000

    Refactored module exports to use wildcard imports.
    Simplified exports by replacing individual item lists with wildcard imports across all modules in `functions/mod.rs`. Added new modules `is_settled` and `status_bits` to exports for consistency.
    
    Signed-off-by: MillieFD <155637089+MillieFD@users.noreply.github.com>

--- src/functions/mod.rs
+++ src/functions/mod.rs
@@ -13,19 +13,23 @@ modification, are permitted provided that the conditions of the LICENSE are met.
 mod channel_enable_state;
 mod home;
 mod identify;
+mod is_settled;
 mod move_absolute;
 mod move_relative;
+mod status_bits;
 mod status_update;
 mod stop;
 mod update_messages;
 
 /* ----------------------------------------------------------------------------- Private Exports */
 
-pub(crate) use channel_enable_state::{req_channel_enable_state, set_channel_enable_state};
-pub(crate) use home::home;
-pub(crate) use identify::identify;
-pub(crate) use move_absolute::{move_absolute, move_absolute_from_params};
-pub(crate) use move_relative::move_relative;
-pub(crate) use status_update::get_u_status_update;
-pub(crate) use stop::{estop, stop};
-pub(crate) use update_messages::{hw_start_update_messages, hw_stop_update_messages};
+pub(crate) use channel_enable_state::*;
+pub(crate) use home::*;
+pub(crate) use identify::*;
+pub(crate) use is_settled::*;
+pub(crate) use move_absolute::*;
+pub(crate) use move_relative::*;
+pub(crate) use status_bits::*;
+pub(crate) use status_update::*;
+pub(crate) use stop::*;
+pub(crate) use update_messages::*;
