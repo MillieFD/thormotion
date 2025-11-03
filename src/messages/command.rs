9091f113ded4e0971226ee167153d1173b181129
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-11-03 19:20:01 +0000

    Refactored `Command` for const generics and modular metadata.
    Split `Command` into `Metadata` and `Command` structs, enhancing modularity and clarity. Introduced const generics for channel size to improve type safety and flexibility. Updated constructor methods and added `sender` accessor for improved usability.
    
    Signed-off-by: MillieFD <155637089+MillieFD@users.noreply.github.com>

--- src/messages/command.rs
+++ src/messages/command.rs
@@ -18,18 +18,22 @@ use crate::messages::Sender;
 /// The max possible command length is therefore six-bytes (header) plus 255 bytes (data payload).
 pub(crate) const CMD_LEN_MAX: usize = 255 + 6;
 
+/// Defines command metadata from the Thorlabs APT Protocol.
+/// Used to construct [`Command`] instances.
+///
+/// 1. Unique two-byte ID
+/// 2. Total command length
+/// 3. Number of device channels
 #[derive(Debug)]
-pub(crate) struct Command {
+pub(crate) struct Metadata<const CHANNELS: usize> {
     /// Unique two-byte identifier for the command
     pub(super) id: [u8; 2],
     /// Total number of bytes in the command
     pub(crate) length: usize,
-    /// A sender for broadcasting command responses to multiple receivers
-    pub(super) sender: Mutex<Option<Sender>>,
 }
 
-impl Command {
-    /// Creates a new [`Command`] with the specified ID and length.
+impl<const CH: usize> Metadata<CH> {
+    /// Creates a new [`Metadata`] with the specified ID and length.
     ///
     /// The total `length` consists of:
     /// - Six-byte message header
@@ -39,19 +43,47 @@ impl Command {
     /// The maximum possible command length is given by [`CMD_LEN_MAX`].
     pub(crate) const fn payload(id: [u8; 2], length: usize) -> Self {
         if length < 6 || length > CMD_LEN_MAX {
-            panic!("Invalid command length");
+            panic!("Invalid command length"); // Compile-time error
         }
-        Self {
-            id,
-            length,
-            sender: Mutex::new(None),
-        }
+        Self { id, length }
     }
 
-    /// Creates a new header-only [`Command`] with the specified ID.
+    /// Creates a new header-only [`Metadata`] with the specified ID.
     ///
     /// Header-only commands are always six bytes long (Thorlabs APT Protocol, Issue 39, Page 34).
     pub(crate) const fn header(id: [u8; 2]) -> Self {
         Self::payload(id, 6)
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct Command<const CH: usize> {
+    /// Unique two-byte identifier for the command
+    pub(super) id: [u8; 2],
+    /// Total number of bytes in the command
+    pub(crate) length: usize,
+    /// A sender for broadcasting command responses to multiple receivers
+    pub(super) senders: [Mutex<Option<Sender>>; CH],
+}
+
+impl<const CH: usize> Command<CH> {
+    /// Construct a new [`Command`] instance from the provided metadata.
+    ///
+    /// Returns a tuple used to construct the [`Dispatcher`][1] sensers [`HashMap`][2].
+    ///
+    /// [1]: crate::messages::dispatcher::Dispatcher
+    /// [2]: ahash::HashMap
+    pub(super) const fn new(m: &Metadata<CH>) -> ([u8; 2], Command<CH>) {
+        let cmd = Command {
+            id: m.id,
+            length: m.length,
+            senders: [const { Mutex::new(None) }; CH],
+        };
+        (m.id, cmd)
+    }
+
+    pub(super) const fn sender(&self, channel: usize) -> &Mutex<Option<Sender>> {
+        let i = if channel <= 1 { 0 } else { channel - 1 };
+        &self.senders[i]
     }
 }
