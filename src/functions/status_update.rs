470a7915c57bc1b2056fe0e56e3ebf6cc75f0fcf
MillieFD <155637089+MillieFD@users.noreply.github.com>
2025-11-03 15:17:21 +0000

    Added detailed debug logging to `get_u_status_update`.
    Introduced debug logs to improve traceability of function execution, including request initiation, response handling, and successful response parsing. Updated function signature to include a constant channel size parameter for enhanced type safety.
    
    Signed-off-by: MillieFD <155637089+MillieFD@users.noreply.github.com>

--- src/functions/status_update.rs
+++ src/functions/status_update.rs
@@ -11,27 +11,32 @@ modification, are permitted provided that the conditions of the LICENSE are met.
 use crate::messages::utils::short;
 use crate::traits::{ThorlabsDevice, UnitConversion, Units};
 
+const REQ_U_STATUS_UPDATE: [u8; 2] = [0x90, 0x04];
+const GET_U_STATUS_UPDATE: [u8; 2] = [0x91, 0x04];
+
 #[doc = include_str!("../documentation/get_status.md")]
-pub(crate) async fn get_u_status_update<A>(device: &A, channel: u8) -> (f64, f64, u32)
+pub(crate) async fn get_u_status_update<A, const CH: usize>(
+    device: &A,
+    channel: usize,
+) -> (f64, f64, u32)
 where
-    A: ThorlabsDevice + UnitConversion,
+    A: ThorlabsDevice<CH> + UnitConversion,
 {
-    const REQ_USTATUSUPDATE: [u8; 2] = [0x90, 0x04];
-    const GET_USTATUSUPDATE: [u8; 2] = [0x91, 0x04];
-    device.check_channel(channel);
-    let response = loop {
-        let rx = device.inner().receiver(&GET_USTATUSUPDATE).await;
-        if rx.is_new() {
-            let command = short(REQ_USTATUSUPDATE, channel, 0);
-            device.inner().send(command).await;
-        }
-        let response = rx.receive().await;
-        if response[6] == channel {
-            break response;
-        }
-    };
-    let position = Units::distance_from_slice(&response[8..12]).decode::<A>();
-    let velocity = Units::velocity_from_slice(&response[12..14]).decode::<A>();
+    log::debug!("{device} CHANNEL {channel} U_STATUS_UPDATE (requested)");
+    // Subscribe to GET_U_STATUS_UPDATE broadcast channel
+    let rx = device.inner().receiver(&GET_U_STATUS_UPDATE, channel).await;
+    if rx.is_new() {
+        // No GET_U_STATUS_UPDATE response pending from the device. Send REQ_U_STATUS_UPDATE.
+        log::debug!("{device} CHANNEL {channel} U_STATUS_UPDATE (is new)");
+        let command = short(REQ_U_STATUS_UPDATE, channel as u8, 0);
+        device.inner().send(command).await;
+    }
+    // Wait for GET_U_STATUS_UPDATE response
+    let response = rx.receive().await;
+    log::debug!("{device} CHANNEL {channel} U_STATUS_UPDATE (responded)");
+    // Parse the GET_U_STATUS_UPDATE response
+    let position = device.decode(Units::distance_from_slice(&response[8..12]));
+    let velocity = device.decode(Units::velocity_from_slice(&response[12..14]));
     let bits = u32::from_le_bytes([response[16], response[17], response[18], response[19]]);
     (position, velocity, bits)
 }
